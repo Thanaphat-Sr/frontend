9fa66a7de1a6e926afaa604dba3b7eb5a01092c1
thanaphat <thanaphat_sriboonma@hcmu.ac.th>
2025-03-11 02:21:42 +0700

    Task 10 การทำ sub routing #จะเห็นว่าทั้งสามไฟล์มีส่วนที่ซ้ำกันอยู่

---
+++ 713-frontend/src/views/event/RegisterView.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import type { Event } from '@/types'
+import eventService from '@/services/EventService'
+
+const event = ref<Event>()
+const route = useRoute()
+
+onMounted(() => {
+  const id = Number(route.params.id)
+  eventService
+    .getEvent(id)
+    .then((response) => {
+      event.value = response.data
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
+</script>
+
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <nav>
+      <router-link :to="{ name: 'event-detail-view', params: { id } }">Details</router-link>
+      |
+      <router-link :to="{ name: 'event-register-view', params: { id } }">Register</router-link>
+      |
+      <router-link :to="{ name: 'event-edit-view', params: { id } }">Edit</router-link>
+    </nav>
+    <p>Register event here</p>
+  </div>
+</template>
