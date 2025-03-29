b6b51dde8f6c5bd5f432ee4a668be2a71fc480a0
thanaphat <thanaphat_sriboonma@hcmu.ac.th>
2025-03-29 22:11:05 +0700

    Task 11 การ mapping Event แก้ไข

--- 713-frontend/src/views/event/RegisterView.vue
+++ 713-frontend/src/views/event/RegisterView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
+import { useRouter } from 'vue-router'
 import type { Event } from '@/types'
 
 const props = defineProps<{
@@ -7,10 +8,19 @@ const props = defineProps<{
   id: string
 }>()
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// Extract the event prop
 const { event } = toRefs(props)
+
+// Use the Vue Router instance
+const router = useRouter()
+
+// Function to handle registration and navigate to the event detail
+const register = () => {
+  router.push({ name: 'event-detail-view', params: { id: props.id } })
+}
 </script>
 
 <template>
   <p>Register event here</p>
+  <button @click="register">Register</button>
 </template>
